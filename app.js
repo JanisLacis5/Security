6905f3e186cc5d2b3b87b7536ee1e9c545fae618
Janis <janislacis06@gmail.com>
2023-06-15 17:57:44 +0300

    Level 3 hashing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from"express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
+import md5 from "md5"
  
 const app = express();
 app.use(express.static("public"));
@@ -15,8 +15,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 const User = new mongoose.model("User", userSchema);
 
 
@@ -32,7 +30,7 @@ app.route("/login")
 
     .post((req, res) => {
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
 
         User.findOne({email: username})
         .then((foundItem) => {
@@ -59,7 +57,7 @@ app.route("/register")
     .post((req, res) => {
         const user = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
         user.save()
         .then(() => {
@@ -71,9 +69,6 @@ app.route("/register")
     })
 
 
-
-
- 
 app.listen(3000, () => {
     console.log("Server started on port 3000.");
 });
